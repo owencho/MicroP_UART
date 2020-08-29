7f3f05ffa306e2ef9e2711ee95e57fe6c5774461
owencho <owencho200@gmail.com>
2020-08-29 18:13:44 +0800

    removed serialslave where does not need so many char *

--- Core/Src/ButtonSM.c
+++ Core/Src/ButtonSM.c
@@ -10,6 +10,7 @@
 #include "Usart.h"
 #include "UsartDriver.h"
 #include "Gpio.h"
+#include "Irq.h"
 #include "String.h"
 #include <stdlib.h>
 
@@ -18,9 +19,8 @@ extern UsartInfo usartInfo[] ;
 BlinkyState buttonState = BUTTON_WAIT;
 char adcRead [] = {0x21,ADC_ADDRESS,0x21, 0xE};
 char ledControl [] = {0x21,LED_ADDRESS , 0x10, 0xE };
-char serialSlave [] = {0x21,SERIAL_ADDRESS , 0x10, 0xE };
+char serialPacket [8] = {0x21,SERIAL_ADDRESS , 0x10, 0xE };
 char adcPacket [4];
-char serialPacket [8];
 int adcValue ;
 void handleButtonSM(){
 	disableIRQ();
@@ -38,7 +38,7 @@ void handleButtonSM(){
 		break;
 
 		case WAIT_ADC_VALUE:
-			strcpy(adcPacket, (info->usartRxBuffer)+DATA_PACKET);
+			strcpy(adcPacket, buffer+DATA_PACKET);
 			if(ledControl[CMD_PACKET] == 0x12){
 				ledControl[CMD_PACKET] = 0x10;
 			}else{
@@ -49,7 +49,6 @@ void handleButtonSM(){
 		break;
 
 		case SEND_CONTROL_LED:
-			memcpy(serialPacket, serialSlave, 4);
 			memcpy(&serialPacket[4], adcPacket, 4);
 			usartSendMessage(MASTER,serialPacket,8);
 			buttonState = SEND_STRING;
